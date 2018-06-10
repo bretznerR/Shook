c8ca45e5c4a237b9fd31c627a45124a41bdec479
Adrien Lemaire <ladrien2@gmail.com>
2018-06-10 22:54:35 +0200

    Add a playlists fragment

--- app/src/main/java/shook/shook/MainActivity.java
+++ app/src/main/java/shook/shook/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                     showFragment(new SearchFragment());
                     return true;
                 case R.id.navigation_notifications:
-                    showFragment(new SearchFragment());
+                    showFragment(new PlaylistsFragment());
                     return true;
             }
             return false;
---
+++ app/src/main/java/shook/shook/PlaylistsFragment.java
@@ -0,0 +1,33 @@
+package shook.shook;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Adrien on 10/06/2018.
+ */
+
+public class PlaylistsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_playlists, container, false);
+        Toolbar topToolBar = (Toolbar)rootView.findViewById(R.id.toolbar);
+        topToolBar.setTitle("Playlists");
+        return rootView;
+    }
+}
+
